d6c84500312aeb9f4a9446c750fd2ec2ffc99e62
HoKirby <lakevely27@gmail.com>
2025-03-12 15:29:14 +0900

    fix: 7 N+1 문제 @EntityGraph 사용해서 해결
    entity graph attribute: EAGER, 그외에는 entity 에 명시한 FetchType or default FetchType

--- src/main/java/org/example/expert/domain/comment/repository/CommentRepository.java
+++ src/main/java/org/example/expert/domain/comment/repository/CommentRepository.java
@@ -1,6 +1,7 @@
 package org.example.expert.domain.comment.repository;
 
 import org.example.expert.domain.comment.entity.Comment;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -9,6 +10,9 @@ import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
-    @Query("SELECT c FROM Comment c JOIN c.user WHERE c.todo.id = :todoId")
+    // fetch join 할 객체의 필드명을 배열로 지정
+    // entity graph attribute: EAGER, 그외에는 entity 에 명시한 FetchType or default FetchType
+    @EntityGraph(attributePaths = "user", type= EntityGraph.EntityGraphType.LOAD)
+    @Query("SELECT c FROM Comment c")
     List<Comment> findByTodoIdWithUser(@Param("todoId") Long todoId);
 }
