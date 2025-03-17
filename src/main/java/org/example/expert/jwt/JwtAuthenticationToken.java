0eb11259f08c47c739dbf0457d6728022692bab4
HoKirby <lakevely27@gmail.com>
2025-03-17 21:30:47 +0900

    refactor: 패키지 정리 refactor

--- src/main/java/org/example/expert/jwt/JwtAuthenticationToken.java
+++ src/main/java/org/example/expert/jwt/JwtAuthenticationToken.java
@@ -1,4 +1,4 @@
-package org.example.expert.config;
+package org.example.expert.jwt;
 import org.example.expert.domain.common.dto.AuthUser;
 import org.springframework.security.authentication.AbstractAuthenticationToken;
 
